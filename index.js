e0f2862ad9c68d0602d44576e59147341d1e3e97
Narender Singh <narender2031@gmail.com>
2020-07-09 15:49:47 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,3 @@
+import WelconeComponent from './src/VodaGameDemoLib/WelcomeComponent';
+
+export { WelconeComponent };
---
+++ src/VodaGameDemoLib/WelcomeComponent.js
@@ -0,0 +1,17 @@
+import react from 'react';
+import { View, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  
+});
+
+const WelcomeComponent = () => {
+  return (
+    <View>
+      <Text>Hello I am from component</Text>
+    </View>
+  )
+}
+
+export default WelcomeComponent;
+
