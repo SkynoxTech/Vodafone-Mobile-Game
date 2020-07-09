51246fa56263394d0b213f5a0188189362b0a800
Narender Singh <narender2031@gmail.com>
2020-07-09 17:14:27 +0530

    update component styles

--- src/VodaGameDemoLib/WelcomeComponent.js
+++ src/VodaGameDemoLib/WelcomeComponent.js
@@ -2,13 +2,17 @@ import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 
 const styles = StyleSheet.create({
-  
+  welcomeContainer: {
+    justifyContent: 'center',
+    alignContent: 'center';
+    flex: 1,
+  }
 });
 
 const WelcomeComponent = () => {
   return (
-    <View>
-      <Text>Hello I am from component</Text>
+    <View style={styles.welcomeContainer}>
+      <Text>Hello I am from component 😃</Text>
     </View>
   )
 }
